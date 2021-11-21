0788929e01531dcf3cc6c2dc2e72608da0eab24c
mondevyat <artemii.origin@yandex.ru>
2021-11-21 20:50:57 +0300

    categories fix

--- services/index.js
+++ services/index.js
@@ -184,3 +184,40 @@ export const getFeaturedPosts = async () => {
 
   return result.posts;
 };
+
+export const getCategoryPost = async (slug) => {
+  const query = gql`
+    query GetCategoryPost($slug: String!) {
+      postsConnection(where: {categories_some: {slug: $slug}}) {
+        edges {
+          cursor
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.postsConnection.edges;
+};
