6182a14f665fa2e0b3368d5928504eb49ddafc42
mondevyat <artemii.origin@yandex.ru>
2021-11-21 20:25:39 +0300

    bug fix

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Link from 'next/Link'
+import Link from 'next/link'
 import { getCategories } from "../services";
 
 const Categories = () => {
