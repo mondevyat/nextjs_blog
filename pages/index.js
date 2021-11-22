9e64aa90dabe5259d5245e89548428e9cb3b1d56
mondevyat <artemii.origin@yandex.ru>
2021-11-22 16:17:04 +0300

    font fix

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home({ posts }) {
         <title>blogify.js — IT-блог</title>
         <meta name="description" content="Mondevyat's IT blog" />
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap"/>
       </Head>
       <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
